c504eb8c9f6d22bd880ba066616f89cb8b1e53b4
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-22 23:00:00 +0300

    Draw a random plot

--- main.lua
+++ main.lua
@@ -1,5 +1,32 @@
+local require_paths =
+  {"?.lua", "?/init.lua", "vendor/?.lua", "vendor/?/init.lua"}
+love.filesystem.setRequirePath(table.concat(require_paths, ";"))
+
+local Plot = require("luaplot.plot")
+require("compat52")
+
+local plot = nil -- luaplot.Plot
+
+function love.load()
+  math.randomseed(os.time())
+
+  plot = Plot:new(0)
+  for _ = 1, 10 do
+    plot:push_with_random_factor(0.2)
+  end
+end
+
 function love.draw()
-  love.graphics.print("Hello, world!", 100, 100)
+  local points = {}
+  for x, y in ipairs(plot) do
+    x = x * 10
+    table.insert(points, x)
+
+    y = y * 100 + 100
+    table.insert(points, y)
+  end
+
+  love.graphics.line(points)
 end
 
 function love.keypressed(key)
