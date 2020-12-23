ef9235d8c4bbbb279246c00121dba63cc3b3ca7f
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-23 23:00:00 +0300

    Draw boundaries

--- main.lua
+++ main.lua
@@ -11,6 +11,8 @@ local horizontal_step = 0
 local horizontal_offset = 0
 local vertical_size = 0
 local vertical_offset = 0
+local boundary_size = 0
+local boundary_step = 0
 local total_dt = 0
 
 function love.load()
@@ -27,9 +29,22 @@ function love.load()
   horizontal_offset = x
   vertical_size = height / 1.5
   vertical_offset = y + (height - vertical_size) / 2
+  boundary_size = width
+  boundary_step = boundary_size / 40
 end
 
 function love.draw()
+  love.graphics.setColor(0.5, 0.5, 0.5)
+  love.graphics.setLineWidth(plot_width / 4)
+  for x = 0, boundary_size, 1.5 * boundary_step do
+    for _, y in ipairs({0, vertical_size}) do
+      love.graphics.line(
+        x + horizontal_offset, y + vertical_offset,
+        x + boundary_step + horizontal_offset, y + vertical_offset
+      )
+    end
+  end
+
   local points = {}
   for x, y in ipairs(plot) do
     x = (x - 1) * horizontal_step + horizontal_offset
