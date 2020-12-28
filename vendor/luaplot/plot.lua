7030041b79ce5a0a75774477c224e8289997543e
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-28 23:00:00 +0300

    Update the luaplot library

--- vendor/luaplot/plot.lua
+++ vendor/luaplot/plot.lua
@@ -3,10 +3,13 @@
 
 local middleclass = require("middleclass")
 local types = require("luaplot.types")
+local maths = require("luaplot.maths")
+local iterators = require("luaplot.iterators")
 
 ---
 -- @table instance
 -- @tfield {number,...} _points
+-- @tfield number _default
 -- @tfield number _minimum
 -- @tfield number _maximum
 
@@ -15,28 +18,32 @@ local Plot = middleclass("Plot")
 ---
 -- @function new
 -- @tparam number length [0, ∞)
+-- @tparam[opt=minimum] number default
 -- @tparam[opt=0] number minimum
 -- @tparam[opt=1] number maximum [minimum, ∞)
 -- @treturn Plot
-function Plot:initialize(length, minimum, maximum)
+function Plot:initialize(length, default, minimum, maximum)
   minimum = minimum or 0
   maximum = maximum or 1
+  default = default or minimum
 
   assert(types.is_number_with_limits(length, 0))
   assert(types.is_number_with_limits(minimum))
   assert(types.is_number_with_limits(maximum, minimum))
+  assert(types.is_number_with_limits(default, minimum, maximum))
 
   self._points = {}
   for _ = 1, length do
-    table.insert(self._points, minimum)
+    table.insert(self._points, default)
   end
+  self._default = default
   self._minimum = minimum
   self._maximum = maximum
 end
 
 ---
 -- It is used for iterating over plot points in Lua 5.3+.
--- @tparam number index
+-- @tparam number index [1, ∞)
 -- @treturn number
 function Plot:__index(index)
   assert(types.is_number_with_limits(index, 1))
@@ -46,25 +53,11 @@ end
 
 ---
 -- It is used for iterating over plot points in Lua 5.2.
--- @treturn func func(points: {number,...}, index: number):
---   nil|(number, number); iterator function
--- @treturn {number,...} points
+-- @treturn iterators.inext iterator function
+-- @treturn {number,...} self._points
 -- @treturn number always zero
 function Plot:__ipairs()
-  local function iterator(points, index)
-    assert(type(points) == "table")
-    assert(types.is_number_with_limits(index, 0))
-
-    local next_index = index + 1
-    local next_item = points[next_index]
-    if next_item == nil then
-      return
-    end
-
-    return next_index, next_item
-  end
-
-  return iterator, self._points, 0
+  return iterators.inext, self._points, 0
 end
 
 ---
@@ -72,12 +65,7 @@ end
 function Plot:push(point)
   assert(types.is_number_with_limits(point))
 
-  if point < self._minimum then
-    point = self._minimum
-  end
-  if point > self._maximum then
-    point = self._maximum
-  end
+  point = maths.clamp(point, self._minimum, self._maximum)
   table.insert(self._points, point)
 end
 
@@ -90,7 +78,7 @@ function Plot:push_with_factor(factor)
   if #self._points ~= 0 then
     last_point = self._points[#self._points]
   else
-    last_point = self._minimum
+    last_point = self._default
   end
 
   local delta_x = 1 -- because it's the next point
@@ -103,7 +91,7 @@ end
 function Plot:push_with_random_factor(factor_limit)
   assert(types.is_number_with_limits(factor_limit))
 
-  local factor = 2 * factor_limit * math.random() - factor_limit
+  local factor = maths.random_in_range(-factor_limit, factor_limit)
   self:push_with_factor(factor)
 end
 
@@ -111,7 +99,7 @@ end
 -- @treturn number
 function Plot:shift()
   local first_point = table.remove(self._points, 1)
-  return first_point or self._minimum
+  return first_point or self._default
 end
 
 return Plot
