36eac21b1bd0e8d12951464df33cd90beae62eec
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-31 23:00:00 +0300

    Update the luaplot library

---
+++ vendor/luaplot/plotiterator.lua
@@ -0,0 +1,53 @@
+---
+-- @classmod PlotIterator
+
+local middleclass = require("middleclass")
+local types = require("luaplot.types")
+local iterators = require("luaplot.iterators")
+local Plot = require("luaplot.plot")
+
+---
+-- @table instance
+-- @tfield Plot _plot
+-- @tfield func(index: number, point: number): any _transformer
+
+local PlotIterator = middleclass("PlotIterator")
+
+---
+-- @function new
+-- @tparam Plot plot
+-- @tparam func(index: number, point: number): any transformer
+-- @treturn PlotIterator
+function PlotIterator:initialize(plot, transformer)
+  assert(types.is_instance(plot, Plot))
+  assert(types.is_callable(transformer))
+
+  self._plot = plot
+  self._transformer = transformer
+end
+
+---
+-- It is used for iterating over plot points in Lua 5.3+.
+-- @tparam number index [1, ∞)
+-- @treturn number
+function PlotIterator:__index(index)
+  assert(types.is_number_with_limits(index, 1))
+
+  local point = self._plot[index]
+  if point == nil then
+    return
+  end
+
+  return self._transformer(index, point)
+end
+
+---
+-- It is used for iterating over plot points in Lua 5.2.
+-- @treturn iterators.inext iterator function
+-- @treturn PlotIterator self
+-- @treturn number always zero
+function PlotIterator:__ipairs()
+  return iterators.inext, self, 0
+end
+
+return PlotIterator
