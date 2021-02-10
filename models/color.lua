c292798d73c49a8a2d615b57e03841b8bdb5c67f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-02-10 23:00:00 +0300

    Add the Color class

---
+++ models/color.lua
@@ -0,0 +1,42 @@
+---
+-- @classmod Color
+
+local middleclass = require("middleclass")
+local types = require("luaplot.types")
+
+local Color = middleclass("Color")
+
+---
+-- @table instance
+-- @tfield number red [0, 1]
+-- @tfield number green [0, 1]
+-- @tfield number blue [0, 1]
+-- @tfield number alpha [0, 1]
+
+---
+-- @function new
+-- @tparam number red [0, 1]
+-- @tparam number green [0, 1]
+-- @tparam number blue [0, 1]
+-- @tparam number alpha [0, 1]
+-- @treturn Color
+function Color:initialize(red, green, blue, alpha)
+  assert(types.is_number_with_limits(red, 0, 1))
+  assert(types.is_number_with_limits(green, 0, 1))
+  assert(types.is_number_with_limits(blue, 0, 1))
+  assert(types.is_number_with_limits(alpha, 0, 1))
+
+  self.red = red
+  self.green = green
+  self.blue = blue
+  self.alpha = alpha
+end
+
+---
+-- @treturn {number,number,number,number}
+--   red, green, blue and alpha values in the range [0, 1]
+function Color:channels()
+  return {self.red, self.green, self.blue, self.alpha}
+end
+
+return Color
