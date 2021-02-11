eafbd5a76376d5c1e9058019b27cd9f40a1c0ed7
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-02-11 23:00:00 +0300

    Add the Stats class

---
+++ models/stats.lua
@@ -0,0 +1,31 @@
+---
+-- @classmod Stats
+
+local middleclass = require("middleclass")
+local types = require("luaplot.types")
+
+local Stats = middleclass("Stats")
+
+---
+-- @table instance
+-- @tfield number normal_time
+-- @tfield number soft_limit_time
+-- @tfield number hard_limit_time
+
+---
+-- @function new
+-- @tparam number normal_time
+-- @tparam number soft_limit_time
+-- @tparam number hard_limit_time
+-- @treturn Stats
+function Stats:initialize(normal_time, soft_limit_time, hard_limit_time)
+  assert(types.is_number_with_limits(normal_time, 0))
+  assert(types.is_number_with_limits(soft_limit_time, 0))
+  assert(types.is_number_with_limits(hard_limit_time, 0))
+
+  self.normal_time = normal_time
+  self.soft_limit_time = soft_limit_time
+  self.hard_limit_time = hard_limit_time
+end
+
+return Stats
