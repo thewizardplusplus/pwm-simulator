30734192e0c747be28ced641c38ba08ee32a2148
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-02-13 23:00:00 +0300

    Add the ui._get_label_width() function

--- ui.lua
+++ ui.lua
@@ -7,6 +7,27 @@ local Color = require("models.color")
 local ui = {}
 
 ---
+-- @tparam number value
+-- @tparam int grid_step
+-- @treturn int
+function ui._get_label_width(value, grid_step)
+  assert(types.is_number_with_limits(value, 0, 100))
+  assert(types.is_number_with_limits(grid_step, 0))
+
+  local label_width
+  if value == 100 then -- three digits
+    label_width = 2.76 * grid_step
+  elseif value >= 10 then -- two digits
+    label_width = 2.4 * grid_step
+  else -- one digit
+    label_width = 2 * grid_step
+  end
+
+  -- rounding to an integer
+  return math.floor(label_width + 0.5)
+end
+
+---
 -- @tparam Color color
 -- @tparam "left"|"right" align
 -- @treturn tab common SUIT widget options
