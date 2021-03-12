0fc497284efee076723f91027e1803a6bbcb78e5
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-12 23:00:00 +0300

    Add the typeutils._catch_error() function

---
+++ typeutils.lua
@@ -0,0 +1,25 @@
+---
+-- @module typeutils
+
+local typeutils = {}
+
+---
+-- @tparam func handler func(): any; function that raises an error
+-- @tparam[opt] {any,...} ... handler arguments
+-- @treturn any successful handler result
+-- @error raised handler error
+function typeutils._catch_error(handler, ...)
+  assert(type(handler) == "function")
+
+  local arguments = table.pack(...)
+  local ok, result = pcall(function()
+    return handler(table.unpack(arguments))
+  end)
+  if not ok then
+    return nil, result
+  end
+
+  return result
+end
+
+return typeutils
