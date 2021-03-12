656cec68df7e23bda08a6408aba5d58bdc842a03
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-12 23:00:00 +0300

    Add the engine config

---
+++ conf.lua
@@ -0,0 +1,33 @@
+local function _is_positive_number(value)
+  return type(value) == "number" and value >= 0
+end
+
+local function _set_title(config, title)
+  assert(type(config) == "table")
+  assert(type(title) == "string")
+
+  config.window.title = title
+  config.identity = string.lower(title)
+end
+
+local function _set_screen_width(config, width, aspect_ratio, prefix)
+  assert(type(config) == "table")
+  assert(_is_positive_number(width))
+  assert(_is_positive_number(aspect_ratio))
+  assert(type(prefix) == "string")
+
+  config.window[prefix .. "width"] = width
+  config.window[prefix .. "height"] = width / aspect_ratio
+end
+
+function love.conf(config)
+  config.version = "11.3"
+
+  config.window.resizable = true
+  config.window.msaa = 8
+
+  _set_title(config, "PWM Simulator")
+  for _, prefix in ipairs({"", "min"}) do
+    _set_screen_width(config, 640, 16 / 10, prefix)
+  end
+end
