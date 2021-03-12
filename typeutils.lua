68180811dc300504d67967a6fdf9aa054a4a78ac
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-12 23:00:00 +0300

    Add the typeutils.load_json() function

--- typeutils.lua
+++ typeutils.lua
@@ -1,7 +1,43 @@
 ---
 -- @module typeutils
 
+local json = require("json")
+local jsonschema = require("jsonschema")
+
 local typeutils = {}
+
+---
+-- @tparam string path
+-- @tparam tab schema JSON Schema
+-- @treturn tab
+-- @error error message
+function typeutils.load_json(path, schema)
+  assert(type(path) == "string")
+  assert(type(schema) == "table")
+
+  local data_in_json, reading_err = love.filesystem.read(path)
+  if not data_in_json then
+    return nil, "unable to read data: " .. reading_err
+  end
+
+  local data, decoding_err = typeutils._catch_error(json.decode, data_in_json)
+  if not data then
+    return nil, "unable to parse data: " .. decoding_err
+  end
+
+  local data_validator, generation_err =
+    typeutils._catch_error(jsonschema.generate_validator, schema)
+  if not data_validator then
+    return nil, "unable to generate the validator: " .. generation_err
+  end
+
+  local ok, validation_err = data_validator(data)
+  if not ok then
+    return nil, "incorrect data: " .. validation_err
+  end
+
+  return data
+end
 
 ---
 -- @tparam func handler func(): any; function that raises an error
