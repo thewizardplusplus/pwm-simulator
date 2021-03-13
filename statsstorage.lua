f09db697469ea833a91b58b88f410c2b55c9a92f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-13 23:00:00 +0300

    Add the StatsStorage class

---
+++ statsstorage.lua
@@ -0,0 +1,30 @@
+---
+-- @classmod StatsStorage
+
+local middleclass = require("middleclass")
+local flatdb = require("flatdb")
+
+local StatsStorage = middleclass("StatsStorage")
+
+---
+-- @table instance
+-- @tfield FlatDB _db
+
+---
+-- @function new
+-- @tparam string path
+-- @treturn StatsStorage
+function StatsStorage:initialize(path)
+  assert(type(path) == "string")
+
+  self._db = flatdb(path)
+  if not self._db.stats then
+    self._db.stats = {
+      normal_time = 0,
+      soft_limit_time = 0,
+      hard_limit_time = 0,
+    }
+  end
+end
+
+return StatsStorage
