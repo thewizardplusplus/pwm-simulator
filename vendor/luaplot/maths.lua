2ebc333b10461230576d49c6d0a399f6dc4942fc
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-02-15 21:44:25 +0600

    Update the `luaplot` package in the dependencies

--- vendor/luaplot/maths.lua
+++ vendor/luaplot/maths.lua
@@ -1,17 +1,49 @@
 ---
 -- @module maths
 
-local types = require("luaplot.types")
+local assertions = require("luatypechecks.assertions")
 
 local maths = {}
+
+---
+-- @tparam number value
+-- @tparam number minimum
+-- @tparam number maximum [minimum, ∞)
+-- @treturn number [minimum, maximum]
+function maths.clamp(value, minimum, maximum)
+  assertions.is_number(value)
+  assertions.is_number(minimum)
+  assertions.is_number(maximum)
+
+  if value < minimum then
+    value = minimum
+  elseif value > maximum then
+    value = maximum
+  end
+
+  return value
+end
+
+---
+-- @tparam number minimum
+-- @tparam number maximum [minimum, ∞)
+-- @tparam number progress [0, 1]
+-- @treturn number [minimum, maximum]
+function maths.lerp(minimum, maximum, progress)
+  assertions.is_number(minimum)
+  assertions.is_number(maximum)
+  assertions.is_number(progress)
+
+  return (maximum - minimum) * progress + minimum
+end
 
 ---
 -- @tparam number minimum
 -- @tparam number maximum [minimum, ∞)
 -- @treturn number [minimum, maximum)
 function maths.random_in_range(minimum, maximum)
-  assert(types.is_number_with_limits(minimum))
-  assert(types.is_number_with_limits(maximum, minimum))
+  assertions.is_number(minimum)
+  assertions.is_number(maximum)
 
   return math.random() * (maximum - minimum) + minimum
 end
